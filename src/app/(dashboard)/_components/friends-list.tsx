054d4c5d831ec61b6ce9700c4055545847049b86
Braulio Fernandes <brauliopf+github@gmail.com>
2024-11-02 19:48:17 -0400

    removes "use client" from not stand-alone components

--- src/app/(dashboard)/_components/friends-list.tsx
+++ src/app/(dashboard)/_components/friends-list.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
